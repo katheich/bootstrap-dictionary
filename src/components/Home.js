41692dc0c689c98708a21ca86dd5601669af7a92
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-23 16:49:59 +0000

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Home = (props) => (
-  <div>
-    Hello, world!
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="title display-1">Looking for words.</h1>
+      <p className="lead">Something something.</p>
+    </div>
   </div>
 )
 
