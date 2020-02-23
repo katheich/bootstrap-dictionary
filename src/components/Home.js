b54a29a4441c0d6da9841de58aeee1ef535a1ebf
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-23 16:30:34 +0000

    basic react webpack setup

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = (props) => (
+  <div>
+    Hello, world!
+  </div>
+)
+
+export default Home
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+const env = process.env.NODE_ENV === 'production' ? (
+  new webpack.EnvironmentPlugin({ ...process.env })
+) : (
+  new Dotenv()
+)
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+    // publicPath: '/bootstrap-dictionary/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(jpg|png|gif)/, use: [{
+        loader: 'url-loader', options: { limit: 5000 }
+      }] }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve('src'),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    env
+  ]
+}
