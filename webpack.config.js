ce029b540065a2f1c55312f83864edcb2b0b5808
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-23 16:35:03 +0000

    bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,25 @@ module.exports = {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+              return [
+                require('autoprefixer')
+              ]
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
       { test: /\.(jpg|png|gif)/, use: [{
         loader: 'url-loader', options: { limit: 5000 }
       }] }
