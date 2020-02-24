e6960bda38454498c98bf8179386f31880f46d58
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-24 17:37:52 +0000

    footer apostrophe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => (
 
   <footer className="row align-items-end text-center small font-weight-light">
     <div className="col">
-    Powered by Merriam-Webster's Collegiate® Dictionary with Audio API.
+    Powered by Merriam-Webster&apos;s Collegiate® Dictionary with Audio API.
       <img src="https://www.dictionaryapi.com/images/info/branding-guidelines/MWLogo_LightBG_120x120_2x.png" alt="Merriam Webster Inc Logo" className="img-thumbnail border-0" width="40" height="40"></img>
     </div>
   </footer>
