8197feb42e36781a4777dee4ef8f8964c5b2fc9c
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-24 13:27:20 +0000

    audio, no results page

--- src/components/WordPage.js
+++ src/components/WordPage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Scroll from 'react-scroll'
 
 const Element = Scroll.Element
 const scroller = Scroll.scroller
-const Link = Scroll.Link
+const ScrollLink = Scroll.Link
 
 
 const WordPage = (props) => {
@@ -13,18 +14,45 @@ const WordPage = (props) => {
 
   useEffect(() => {
     axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${props.match.params.q}?key=${process.env.apiKey}`)
-      .then(resp => setData(resp.data))
-      .catch(err => console.log(err))
+      .then(resp => {
+        console.log('RESPONSE', resp)
+        setData(resp.data)
+      })
+      .catch(err => console.log('ERROR', err))
   }, [])
+
+  function determineAudioFile(audio) {
+    let subdirectory = ''
+
+    if (audio.startsWith('bix')) {
+      subdirectory = 'bix'
+    } else if (audio.startsWith('gg')) {
+      subdirectory = 'gg'
+    } else if (Number.isInteger(audio[0]) || !audio[0].match(/[a-zA-Z]/)) {
+      subdirectory = 'number'
+    } else {
+      subdirectory = audio[0]
+    }
+
+    const path = `https://media.merriam-webster.com/soundc11/${subdirectory}/${audio}.wav` 
+    return path
+  }
+
+  function playSound(e, audio) {
+    e.preventDefault()
+    const path = determineAudioFile(audio)
+    audio = new Audio(path)
+    audio.play()
+  }
 
   return (
     <div>
-      {console.log(data)}
+      {console.log('DATA', data)}
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="text-capitalize display-5 title">{props.match.params.q}</h1>
 
-          {data.length === 0 ? 'Sorry, no match could be found.' : 
+          {data.length === 0 || !data[0].hwi ? 'Sorry, no match could be found.' : 
           
             <div className="font-weight-light">
               [{data[0].hwi.prs[0].mw}] 
@@ -38,14 +66,36 @@ const WordPage = (props) => {
       </div>
       
       <div className="container">
+
+        {data.length !== 0 && data[0].hwi ? console.log('good data') : 
+          <>
+            <div className="h4 title">Did you mean any of the following?</div>
+          
+            <div className="list-group list-group-horizontal d-flex flex-wrap">
+              {data.map((elem, i) => {
+                return <Link
+                  key={i}
+                  to={`/reroute/${elem}`}
+                  className="list-group-item justify-content-between flex-grow-1 border-0"
+                >
+                  {elem}
+                </Link>
+              })}
+            </div>
+          </>
+        }
+
+        
+
+
         <div className="row">
           <div className="col-4">
             <div className="list-group list-group-flush sticky-top" id="list" >
-              {data.length === 0 ? '' : 
+              {data.length === 0 || !data[0].hwi ? '' : 
             
                 data.map((elem, i) => {
 
-                  return <Link 
+                  return <ScrollLink 
                     key={i} 
                     spy={true}
                     smooth={true}
@@ -53,7 +103,7 @@ const WordPage = (props) => {
                     to={`myScrollToElement${i}`}>
                     <span className="font-weight-bold">
                       {elem.hwi.hw}</span>, {elem.fl}
-                  </Link>
+                  </ScrollLink>
                 })
               }
             </div>
@@ -61,7 +111,7 @@ const WordPage = (props) => {
           <div className="col-8" data-spy="scroll" data-target="#list" id="definitions">
             <div data-offset="0" className="scrollspy-example">
 
-              {data.length === 0 ? '' : 
+              {data.length === 0 || !data[0].hwi ? '' : 
               
                 data.map((elem, i) => {
                   
@@ -72,7 +122,17 @@ const WordPage = (props) => {
                       </div>
                       <div className="h6 mt-0 mb-3"><em>{elem.fl}</em></div>
 
-                      {elem.hwi.prs && <div>[{elem.hwi.prs[0].mw}]</div>}
+
+                      {elem.hwi.prs && <div className="flex-row">
+                        <div className="d-inline-flex">
+                        [{elem.hwi.prs[0].mw}]
+                        </div>
+
+                        <button type="button" className="btn btn-link p-0 ml-2" onClick={e => playSound(e, elem.hwi.prs[0].sound.audio)}>
+                          <i className="fas fa-volume-up"></i>
+                        </button>
+                      </div>}
+
 
                       {elem.lbs && elem.lbs.map((label, i) => {
                         return <div className="small font-weight-light" key={i}>
