49d3f5e65519eba329b9c6ed71ac74e4f1e9402e
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-24 10:33:59 +0000

    title typing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const top10 = ['pretentious', 'ubiquitious', 'love', 'cynical', 'apathetic', 'conunndrum', 'albeit', 'ambiguous', 'integrity', 'affect']
 
 import WordSearch from './WordSearch'
 
-const Home = (props) => (
-  <div>
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container typewriter">
-        <h1 className="title display-3">
-          <p>Looking for words?</p>
-        </h1>
-        <p className="lead mt-5">Here you can find some.</p>
+const Home = (props) => {
+
+  const [title, setTitle] = useState('')
+
+  function autoType(word, typingSpeed){
+    var text = word.trim().split('')
+    var amntOfChars = text.length
+    var newString = ''
+    setTimeout(function(){
+      for (var i = 0; i < amntOfChars; i++) {
+        (function(i,char){
+          setTimeout(function() {        
+            newString += char
+            setTitle(newString)
+          }, i * typingSpeed)
+        })( i + 1 ,text[i])
+      }
+    }, 1500)
+  }
+  
+
+  useEffect(() => {
+    autoType('Looking for a word?', 150)
+  }, [])
+  
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container typewriter">
+          <h1 className="title display-3">
+            <div className="type-js">{title}<span className="cursor">|</span></div>
+          </h1>
+          <p className="lead mt-5">Here you can find some.</p>
+        </div>
+      </div>
+      <div className="container">
+        <WordSearch props={props} size={'large'} />
       </div>
     </div>
-    <div className="container">
-      <WordSearch props={props} size={'large'} />
-    </div>
-  </div>
-)
+  )
+}
 
 export default Home
